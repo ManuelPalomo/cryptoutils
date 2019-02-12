6ee84c6a29a9bb5b148792f82b6fbbc49f0a63fc
Palomo Ramon <mpalomo@minsait.com>
2019-02-12 13:35:56 +0100

    Added coincidences and a few other helper functions

--- cryptoutils.py
+++ cryptoutils.py
@@ -1,18 +1,23 @@
 from operator import itemgetter
 import re
 
+# region general
+
+
 def analyze_letter_frequency(text: str) -> dict:
     stripped_text = clean_non_alphabetic_characters(text)
     stripped_text_length = len(stripped_text)
 
     occurences = get_occurences(stripped_text)
-    return get_frequencies(occurences,stripped_text_length)
+    return get_frequencies(occurences, stripped_text_length)
 
-def clean_non_alphabetic_characters(text:str) -> str:
+
+def clean_non_alphabetic_characters(text: str) -> str:
     regex = re.compile('[^a-z]')
 
     stripped_text = text.replace(" ", "").lower()
-    return regex.sub('',stripped_text)
+    return regex.sub('', stripped_text)
+
 
 def get_occurences(text: str) -> dict:
     occurences = {}
@@ -26,9 +31,39 @@ def get_occurences(text: str) -> dict:
 
 def get_frequencies(occurences: dict, text_length: int) -> dict:
     frequencies = {}
-    for key,value in occurences.items():
+    for key, value in occurences.items():
         frequencies[key] = value/text_length
     return frequencies
 
-def sort_dictionary_by_value(dictionary:dict) -> list:
-    return sorted(dictionary.items(),key=itemgetter(1),reverse=True)
+
+def get_index_of_coincidence(text: str) -> float:
+    occurrences = get_occurences(text)
+    number_of_occurrences = len(text)
+
+    numerator = 0
+    for key, value in occurrences.items():
+        numerator += value*(value-1)
+
+    denominator = number_of_occurrences*(number_of_occurrences-1)
+
+    return numerator/denominator
+
+
+def sort_dictionary_by_value(dictionary: dict) -> list:
+    return sorted(dictionary.items(), key=itemgetter(1), reverse=True)
+
+
+def separate_string_with_separator(text: str, group_number: int, separator: str) -> str:
+    splitted_groups = [text[i:i+group_number]
+                       for i in range(0, len(text), group_number)]
+    return separator.join(splitted_groups)
+
+# endregion
+
+# region Baconian
+
+
+def transform_to_binary_by_token(text: str, zero_token: str, one_token: str) -> str:
+    return text.replace(zero_token, '0').replace(one_token, '1')
+
+# endregion
